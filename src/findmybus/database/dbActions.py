fedb1e6c836696d48fc2a60d05bed48b54cf9978
pmourafr <pedrofran682@gmail.com>
2025-11-29 20:44:20 -0300

    adding option to delete all records

--- src/findmybus/database/dbActions.py
+++ src/findmybus/database/dbActions.py
@@ -1,19 +1,21 @@
 from typing import Any
-from sqlalchemy import Engine, Sequence, select
+from sqlalchemy import Engine, Sequence, delete, select
 from sqlalchemy.orm import Session
 from sqlalchemy.dialects.postgresql import insert
 from findmybus.Models.orm import Base, BusesStations, Positions, Routes
 
+import logging
+log = logging.getLogger()
 
 def upinsert(engine: Engine,
              orm_table: Base,
              list_of_dict_insert: list[dict[str, Any]],
              exclude_cols: dict[str] = {},
              index_elements = None) -> None:
+    log.info("Executing upinsert action")
     with Session(engine) as session:
         insert_stmt = insert(orm_table).values(list_of_dict_insert)
         exclude_cols = exclude_cols 
-
         on_conflict_set = {
             column.name: getattr(insert_stmt.excluded, column.name)
             for column in orm_table.__table__.columns
@@ -26,9 +28,16 @@ def upinsert(engine: Engine,
         session.execute(do_update_stmt)
         session.commit()
 
+def erase_table_entries(engine: Engine, orm_table: Base):
+    log.info("Deleteing all entries from db")
+    with Session(engine) as session:
+        delete_stmt = delete(orm_table)
+        session.execute(delete_stmt)
+        session.commit()
 
 def get_buses_position(engine: Engine,
                      line: str) -> Sequence[Positions]:
+    log.info("Getting buses positions")
     with Session(engine) as session:
         return session.scalars(
             select(Positions).where(Positions.line == line)).all()
@@ -36,11 +45,13 @@ def get_buses_position(engine: Engine,
 
 def get_bus_route(engine: Engine,
                   line: str) -> Sequence[Routes]:
+    log.info("Getting buses routes")
     with Session(engine) as session:
         return session.scalars(
             select(Routes).where(Routes.line == line)).all()
     
 def get_bus_station(engine: Engine) -> Sequence[Routes]:
+    log.info("Getting buses stations")
     with Session(engine) as session:
         return session.scalars(
             select(BusesStations)).all()
