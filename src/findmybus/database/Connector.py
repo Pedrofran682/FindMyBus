4b924b943be11a95afacd8020de772299f6b0563
pmourafr <pedrofran682@gmail.com>
2025-11-30 14:23:38 -0300

    fixing db connection

--- src/findmybus/database/Connector.py
+++ src/findmybus/database/Connector.py
@@ -7,23 +7,40 @@ from sqlalchemy import Engine
 
 
 class Connector:
-    def __init__(self):
-        # TODO: Remove load_dotenv from here
+    def __init__(self, scope: str):
         load_dotenv()
+        self._validate_tables()
+        self._get_user_credentials(scope)
         self.engine = create_engine(self._get_url_connection(),
                                     pool_pre_ping=True)
-        Base.metadata.create_all(self.engine)
-        
 
-    def _get_url_connection(self):
+
+    def _get_url_connection(self) -> URL:
         return URL.create(
             "postgresql+pg8000",
-            username=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
+            username=os.getenv(self.username),
+            password=os.getenv(self.password),
             host=os.getenv("DB_HOST"),
             port=os.getenv("DB_PORT"),
-            database=os.getenv("DB_NAME")
+            database=os.getenv("POSTGRES_DB")
             )
-    
+
     def get_db_engine(self) -> Engine:
         return self.engine
+    
+
+    def _get_user_credentials(self, scope: str):
+        if scope == "db":
+            self.username = "POSTGRES_WORKER_USER"
+            self.password = "POSTGRES_WORKER_PASSWORD"
+        if scope == "ui":
+            self.username = "POSTGRES_UI_USER"
+            self.password = "POSTGRES_UI_PASSWORD"
+
+
+    def _validate_tables(self):
+        self.username = "POSTGRES_USER"
+        self.password = "POSTGRES_PASSWORD"
+        engine = create_engine(self._get_url_connection(),
+                               pool_pre_ping=True)
+        Base.metadata.create_all(engine)
--- src/findmybus/ui/utils.py
+++ src/findmybus/ui/utils.py
@@ -15,9 +15,9 @@ def _get_marker_symbol(icon_path: str):
         icon_size=(24,24))
 
 
-@st.cache_resource(ttl=DELTA_REFRESH * 10)
+@st.cache_resource()
 def get_db_engine() -> Engine:
-    conn = Connector()
+    conn = Connector("ui")
     return conn.get_db_engine()
 
 def unixTime2DateTime(unixTime: int): return datetime.fromtimestamp(int(unixTime)/ 1000)
@@ -28,7 +28,7 @@ def get_diff_time_status(bus_unixTime: int) -> str:
     delta = (now - postion_time).total_seconds() 
     return f"Atualizado em {delta:.2f} s"
 
-@st.cache_resource(ttl=DELTA_REFRESH)
+@st.cache_data(ttl=DELTA_REFRESH)
 def get_fg_bus_location(line: str) -> folium.FeatureGroup:
     positions = get_buses_position(get_db_engine(), line)
     marker_group = folium.FeatureGroup(name="BusPosition")
@@ -47,7 +47,7 @@ def get_fg_bus_location(line: str) -> folium.FeatureGroup:
     return marker_group
 
 COLORS = ["green", "red"]
-@st.cache_resource(ttl=(DELTA_REFRESH * 5))
+@st.cache_data(ttl=(DELTA_REFRESH * 5))
 def get_fg_bus_route(line: str) -> folium.FeatureGroup:
     routes = get_bus_route(get_db_engine(), line)
     display_info = {"destination":[],
@@ -84,7 +84,7 @@ def get_fg_bus_route(line: str) -> folium.FeatureGroup:
 #     return marker_group
 
 
-@st.cache_resource(ttl=DELTA_REFRESH)
+@st.cache_data(ttl=DELTA_REFRESH)
 def get_bus_info(line: str):
     # fg_bus_station = get_fg_bus_stations()
     fg_bus_position = get_fg_bus_location(line)
